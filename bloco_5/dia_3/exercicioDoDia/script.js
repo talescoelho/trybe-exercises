b23141443f5f939fd52bcf5546605f10f12f93f4
Tales Coelho <tales-coelho@live.com>
2021-04-07 19:10:22 -0300

    exercicios 4

--- bloco_5/dia_3/exercicioDoDia/script.js
+++ bloco_5/dia_3/exercicioDoDia/script.js
@@ -58,13 +58,13 @@ createDays();
 // Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
 // Adicione a este botão a ID "btn-holiday" .
 // Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
-function createButton(str) {
+function createButton(str, idName) {
   let button = document.createElement('button');
-  button.id = 'btn-holiday';
+  button.id = idName;
   button.innerText = str;
   document.querySelector('.buttons-container').appendChild(button);
 }
-createButton('Feriados');
+createButton('Feriados', 'btn-holiday');
 
 // Exercício 3:
 // Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
@@ -87,6 +87,8 @@ buttonHoliday.addEventListener('click', function(){
 // Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
 // Adicione a este botão o ID "btn-friday" .
 // Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+createButton('Sexta-feira', 'btn-friday');
+
 // Exercício 5:
 // Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
 // É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
