08c972beeb534d46bcaa7486840e3ac969a1121a
Tales Coelho <tales-coelho@live.com>
2021-04-26 16:45:57 -0300

    parte 2 exercicio 2

--- bloco-7/dia-1/parte-2/script.js
+++ bloco-7/dia-1/parte-2/script.js
@@ -1,3 +1,4 @@
+// Fatorial
 // const fatorial = (num) => {
 //   if (num <= 1) {
 //     return 1;
@@ -5,7 +6,22 @@
 //     return num * fatorial(num - 1);
 //   }
 // };
+// Fatorial
+// const fatorial = (num) => num <= 1 ? 1 : num * fatorial(num - 1);
+// console.log(fatorial(6));
 
-const fatorial = (num) => num <= 1 ? 1 : num * fatorial(num - 1);
+// Maior palavra
+// function longestWord(frase) {
+//   let arr = frase.split(' ');
+//   let count = 0;
+//   for (let index = 1; index < arr.length; index++) {
+//     if (arr[index].length > arr[index - 1].length) {
+//       count = index;
+//     }
+//   }
+//   return arr[count];
+// };
+// Maior palavra
+// const longestWord = (frase) => frase.split(' ').sort((a, b) => b.length - a.length)[0];
+// console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
 
-console.log(fatorial(6));
