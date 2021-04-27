defc772ccf249c2049d6665d5140deb01704fd8b
Tales Coelho <tales-coelho@live.com>
2021-04-27 14:38:29 -0300

    bloco 7 exercicio bonus 1

--- bloco-7/dia-2/exercicioDoDia/script2.js
+++ bloco-7/dia-2/exercicioDoDia/script2.js
@@ -18,39 +18,48 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// 1 Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 const insertTurn = (lesson, turn, chave) => lesson[turn] = chave;
 insertTurn(lesson2, 'turno', 'manhã');
 console.log(lesson2);
 
-// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// 2 Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 const objKeys = obj => console.log(Object.keys(obj));
 objKeys(lesson2);
 
-// Crie uma função para mostrar o tamanho de um objeto.
+// 3 Crie uma função para mostrar o tamanho de um objeto.
 const objKeysLength = obj => console.log(Object.keys(obj).length);
 objKeysLength(lesson1);
 
-// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+// 4 Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 const objValues = obj => console.log(Object.values(obj));
 objValues(lesson2);
 
-// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+// 5 Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 const allLessons = {};
 Object.assign(allLessons, {lesson1, lesson2, lesson3});
 console.table(allLessons);
 
-// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+// 6 Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 const objAllStudents = Object.keys(allLessons).map(a => allLessons[a].numeroEstudantes).reduce((a,b)=>a+b);
 console.log(objAllStudents);
 
-// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+// 7 Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 const getValueByNumber = (lesson, value) => allLessons[Object.keys(allLessons)[value]].materia;
 console.log(getValueByNumber(lesson1, 2));
 
-// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+// 8 Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
 const verifyPair = (lesson, key, value) => lesson[key] === value ? true : false;
 // Output: true,
 console.log(verifyPair(lesson3, 'turno', 'noite'));
 // Output: false
 console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+
+// Bônus
+// 1 Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5.
+// allLessons
+const studentsMateria = (value) => Object.keys(allLessons).map(a => allLessons[a].materia === value ? allLessons[a].numeroEstudantes : 0).reduce((a,b) => a+b);
+console.log(studentsMateria('Matemática'));
+
+// 2 Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
+
