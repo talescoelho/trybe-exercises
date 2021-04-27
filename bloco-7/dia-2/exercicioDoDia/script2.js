deea807e81fe3be13a4b238852641dc9e94c44c5
Tales Coelho <tales-coelho@live.com>
2021-04-27 14:17:40 -0300

    bloco 7 exercicio do dia ex 5

--- bloco-7/dia-2/exercicioDoDia/script2.js
+++ bloco-7/dia-2/exercicioDoDia/script2.js
@@ -34,3 +34,8 @@ objKeysLength(lesson1);
 // Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 const objValues = obj => console.log(Object.values(obj));
 objValues(lesson2);
+
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+const allLessons = {};
+Object.assign(allLessons, {lesson1, lesson2, lesson3});
+console.table(allLessons);
