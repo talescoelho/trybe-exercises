db4a26ddc631e7d3cec965a240cf5d920786b458
Tales Coelho <tales-coelho@live.com>
2021-04-28 19:53:38 -0300

    bloco 7 dia 3 bonus 3

---
+++ bloco-7/dia-3/exercicioDoDia/scriptB3.js
@@ -0,0 +1,13 @@
+// Escreva a função removeMiddle para passar nos testes já implementados.
+
+const assert = require('assert');
+// escreva a função removeMiddle aqui
+const removeMiddle = words => words.splice(Math.floor(words.length / 2), 1);
+
+const words = ['mouse', 'giraffe', 'queen', 'window', 'bottle'];
+const expectedWords = ['mouse', 'giraffe', 'window', 'bottle'];
+const expectedOutput = ['queen'];
+const output = removeMiddle(words);
+
+assert.deepStrictEqual(output, expectedOutput);
+assert.deepStrictEqual(words, expectedWords);
