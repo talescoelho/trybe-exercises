e0e11b6acf4bf6abf1a425c98ee06c53986960c5
Tales Coelho <tales-coelho@live.com>
2021-04-28 18:30:14 -0300

    bloco 7 dia 3 exercicio 7

---
+++ bloco-7/dia-3/exercicioDoDia/script7.js
@@ -0,0 +1,12 @@
+// Escreva a função wordLengths para passar nos testes já implementados.
+
+const assert = require('assert');
+// escreva a função wordLengths aqui
+const wordLengths = words => words.map(a => a.length);
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
