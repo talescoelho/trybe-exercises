2eb980010027b20685773d867defbf522b747508
Tales Coelho <tales-coelho@live.com>
2021-04-28 18:43:07 -0300

    bloco 7 dia 3 exercicio 9

---
+++ bloco-7/dia-3/exercicioDoDia/script9.js
@@ -0,0 +1,20 @@
+// Escreva a função findTheNeedle para passar nos testes já implementados.
+
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+const findTheNeedle = (words, word) => words.findIndex(a => a === word);
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
