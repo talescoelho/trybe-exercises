b9fd9b74be91598234ff8bf7a7b5fff607764103
Tales Coelho <tales-coelho@live.com>
2021-04-28 12:44:42 -0300

    bloco 7 dia 3 exercicio 2

--- bloco-7/dia-3/exercicioDoDia/script2.js
+++ bloco-7/dia-3/exercicioDoDia/script2.js
@@ -14,4 +14,6 @@ function myRemove(arr, item) {
   return newArr;
 }
 // implemente seus testes aqui
-assert.strictEqual
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
