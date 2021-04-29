bc1ba2286e9d9c4ddd14f51278efea896c05fdd3
Tales Coelho <tales-coelho@live.com>
2021-04-29 12:19:43 -0300

    adicionando comentários nos códigos 10 ao 13

--- bloco-7/dia-3/exercicioDoDia/script11.js
+++ bloco-7/dia-3/exercicioDoDia/script11.js
@@ -1,13 +1,18 @@
 // Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 const assert = require('assert');
 
+// criando uma função para trocar as vogais minúsculas para uma sequencia de números começando no 1
 const removeVowels = (word) => {
+  // colocando cada caractere de uma string em um array
   const characters = word.split('');
+  // criando o array que vai ser retornado
   const results = [];
-
+  // criando um contador que será inserido seu valor no lugar das vogais
   let count = 1;
 
+  // um for para verificar e trocar cada vogal
   for (let index = 0; index < characters.length; index += 1) {
+    // verificar se meu caractere é uma vogal minúscula
     if (
       characters[index] === 'a' ||
       characters[index] === 'o' ||
@@ -15,17 +20,22 @@ const removeVowels = (word) => {
       characters[index] === 'e' ||
       characters[index] === 'u'
     ) {
+      // inserindo no meu array o valor do contador
       results.push(count);
+      // somando +1 para meu contador
       count += 1;
     } else {
+      // inserindo as consoantes e as vogaís maiúsculas no meu array
       results.push(characters[index]);
     }
   }
+  // retornando meu array o transformando em uma string
   return results.join('');
 };
 
-
+// criando a string que será modificada
 const parameter = 'Dayane';
+// criando o resultado esperado da minha função
 const result = 'D1y2n3';
-
+// verificando se a saída da minha função é a esperada
 assert.deepStrictEqual(removeVowels(parameter), result);
--- bloco-7/dia-3/exercicioDoDia/script12.js
+++ bloco-7/dia-3/exercicioDoDia/script12.js
@@ -1,17 +1,24 @@
 // Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 const assert = require('assert');
-
+// criando uma função que vai pegar os números do array maior que 10 e os retornar
 const greaterThanTen = (array) => {
+  // criando um novo array
   let results = [];
+  // criando um for com o array que foi enviado
   for (let index = 0; index < array.length; index += 1) {
+    // verificando se o item do array é maior que 10
     if (array[index] > 10) {
+      // inserindo esse item no meu novo array
       results.push(array[index]);
     }
   }
+  // retornando meu novo array
   return results;
 };
 
+// criando meu array que será verificado
 const parameter = [4, 10, 32, 9, 21];
+// criando um array com o meu valor esperado
 const result = [32, 21];
-
+// verificando se a saída da minha função é a esperada
 assert.deepStrictEqual(greaterThanTen(parameter), result);
