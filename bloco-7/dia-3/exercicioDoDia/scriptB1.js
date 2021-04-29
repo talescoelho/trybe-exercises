d501f507020f10d1eddd347a188f094122a77446
Tales Coelho <tales-coelho@live.com>
2021-04-28 21:00:47 -0300

    bloco 7 dia 3 bonus 1 corrigindo variavel não utilizada

--- bloco-7/dia-3/exercicioDoDia/scriptB1.js
+++ bloco-7/dia-3/exercicioDoDia/scriptB1.js
@@ -3,7 +3,6 @@ const assert = require('assert');
 function getChange(payable, paid) {
   const coins = [200, 100, 50, 20, 10, 5, 2, 1];
   const change = [];
-  const { length } = coins;
   let remaining = paid - payable;
   if (remaining < 0) {
     throw Error('paid value is not enough');
