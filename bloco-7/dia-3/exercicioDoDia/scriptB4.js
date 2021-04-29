740766bad840ab5078fd28304fd6ec05b9a88d2a
Tales Coelho <tales-coelho@live.com>
2021-04-29 13:35:00 -0300

    adicionando comentários nos códigos bônus

--- bloco-7/dia-3/exercicioDoDia/scriptB4.js
+++ bloco-7/dia-3/exercicioDoDia/scriptB4.js
@@ -2,17 +2,24 @@
 // Lembre-se: testes pequenos e numerosos! Escreva um por vez e vá corrigindo a função aos poucos.
 const assert = require('assert');
 
+// estamos fazendo uma função para pegar o maior número de um array
 const getLargestNumber = (array) => {
+    // aqui estamos criando uma variavel que vai receber o primeiro item do array
     let largestNumber = array[0];
+    // criando um for para percorrer todo o array enviado via parametro
     for (let index = 1; index < array.length; index += 1) {
+        // verificando se o item do array é maior que o item em nossa variavel 'largestNumber'
         if (largestNumber < array[index]) {
+            // se ele for maior inserimos ele na nossa variavel 'largestNumber'
             largestNumber = array[index];
         }
     }
+    // aqui retornamos o maior numero do array
     return largestNumber;
 }
-
+// criando um array de numeros
 const parameter = [45, 8, 2, 50, 1, 7, 99];
+// o resultado esperado da minha função
 const result = 99;
-
+// fazendo a verificação se minha função está retornando o valor esperado
 assert.deepStrictEqual(getLargestNumber(parameter), result);
--- bloco-7/dia-3/exercicioDoDia/scriptB5.js
+++ bloco-7/dia-3/exercicioDoDia/scriptB5.js
@@ -1,15 +1,22 @@
+// função para verificar o password
 const verify = pass => {
+  // aqui estamos fazendo a verificação se:
+  // o passowrd não está vazio e tem mais de 8 letras
+  // existe letra MAIÚSCULA, minúscula e se existe um número
   if (pass.length >= 8 &&
     pass.split('').find(a => a === a.toUpperCase() && a === a.toLowerCase() && !isNaN(a))) {
+      // aqui estamos retornando se o password seria aceito
       return 'Ok';
     } else {
+      // aqui retornamos se ele não for aceiro
       return 'Not Ok';
     };
 };
+// Aqui criamos a função que vai efetuar a chamada da função que verifica o password
 const passwordVerifier = password => verify(password);
-
+// aqui criamos uma variavel com o valor do password
 passwordVerifier('tal3esaT');
-
+// aqui efetuamos a inserção do assert
 const assert = require('assert');
-
+// aqui estamos fazendo a verificação se o password informado é valido
 assert.deepStrictEqual(passwordVerifier('tal3esaT') , 'Ok');
