3f7daafa878d1b11963820ef3ab5278e25714e6c
Tales Coelho <tales-coelho@live.com>
2021-04-29 11:12:53 -0300

    adicionando comentários nos códigos 8 ao 9

--- bloco-7/dia-3/exercicioDoDia/script8.js
+++ bloco-7/dia-3/exercicioDoDia/script8.js
@@ -2,10 +2,16 @@
 
 const assert = require('assert');
 // escreva a função sumAllNumbers aqui
+// função para somar todos os itens do array
 const sumAllNumbers = numbers => numbers.reduce((a,b) => a+b);
+// criando um array de números
 const numbers = [9, 23, 10, 3, 8];
+// criando uma variavel com um valor esperado da soma de nosso array
 const expected = 53;
+// recuperando a saída do nosso array
 const output = sumAllNumbers(numbers);
 
+// verificando se nosso array existe
 assert.strictEqual(typeof sumAllNumbers, 'function');
+// verificando se a saída da nossa função é a esperada
 assert.strictEqual(output, expected);
