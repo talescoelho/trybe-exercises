07dbb22930a2fa781e18d4b61272955801327026
Tales Coelho <tales-coelho@live.com>
2021-05-06 10:29:30 -0300

    Bloco 8 dia 5 Exercício de fixação 3

---
+++ bloco-8/dia-5/exercicios/arrayDestructing.js
@@ -0,0 +1,9 @@
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+console.log(saudacoes[1](saudacoes[0])); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [el1, el2] = saudacoes;
+
+console.log(el1, (el2[0]));
