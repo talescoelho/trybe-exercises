b6afcbb9501a58e166be4e77407d6d8cf561a55b
Tales Coelho <tales-coelho@live.com>
2021-05-06 10:42:06 -0300

    Bloco 8 dia 5 Exercício de fixação 4

--- bloco-8/dia-5/exercicios/arrayDestructing.js
+++ bloco-8/dia-5/exercicios/arrayDestructing.js
@@ -12,10 +12,8 @@ let comida = 'gato';
 let animal = 'água';
 let bebida = 'arroz';
 
-const arr = [];
-arr.push(comida, animal, bebida);
 
-[ animal, bebida, comida ] = arr;
+[ animal, bebida, comida ] = [comida, animal, bebida];
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 
@@ -24,5 +22,7 @@ arr.push(comida, animal, bebida);
 let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[,,, ...numerosPares] = numerosPares;
 
 console.log(numerosPares); // [6, 8, 10, 12]
