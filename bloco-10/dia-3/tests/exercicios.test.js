e959d03e0cc5ee398080d3e9142e221a00621899
Tales Coelho <tales-coelho@live.com>
2021-05-20 18:53:03 -0300

    Exercicio 1 finalizado

--- bloco-10/dia-3/tests/exercicios.test.js
+++ bloco-10/dia-3/tests/exercicios.test.js
@@ -1,16 +1,28 @@
 // #1 Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
-const functionNumber = require('../exercicioDoDia/randNumber');
+let functionNumber = require('../exercicioDoDia/randNumber');
 
 test("#1 Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.", () => {
-  functionNumber.randNumber = jest.fn()
-    .mockReturnValue(10);
-    functionNumber.randNumber();
-  expect(functionNumber.randNumber).toHaveBeenCalled();
-  expect(functionNumber.randNumber()).toBe(10);
-  expect(functionNumber.randNumber).toHaveBeenCalledTimes(2);
+  const mockRandNumber = jest
+    .spyOn(functionNumber, "randNumber")
+    .mockReturnValue(20);
+    
+    mockRandNumber();
+  expect(mockRandNumber).toHaveBeenCalled();
+  expect(mockRandNumber()).toBe(10);
+  expect(mockRandNumber).toHaveBeenCalledTimes(2);
 });
 
 // #2 Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
+
+// test("#2 Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.", () => {
+//   functionNumber.randNumber
+//     .mockImplementation((a, b) => a / b);
+
+//   functionNumber.randNumber(4 / 2);
+//   expect(functionNumber.randNumber).toHaveBeenCalled();
+//   expect(functionNumber.randNumber(4 / 2)).toBe(2);
+//   expect(functionNumber.randNumber).toHaveBeenCalledTimes(2);
+// });
 
 // #3 Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
 
