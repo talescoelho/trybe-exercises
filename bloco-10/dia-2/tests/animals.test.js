3d9476bce5eb7c89fad1c7daf47e42ac0b93374c
Tales Coelho <tales-coelho@live.com>
2021-05-20 08:40:27 -0300

    Exercicio de fixação 1 Teste assincrono

--- bloco-10/dia-2/tests/animals.test.js
+++ bloco-10/dia-2/tests/animals.test.js
@@ -1,8 +1,9 @@
-const { getListAnimals } = require('../exercicioDoDia/animals');
+const { getListAnimals } = require('../exercicioDeFixacao/findAnimalsByType');
 
 describe('Quando o tipo do animal, existe', () => {
   test('Retorne a lista de animais', () => {
-    getListAnimals('Dog').then(listDogs => {
+    expect.assertions(2);
+    return getListAnimals('Dog').then(listDogs => {
       expect(listDogs[0].name).toEqual('Dorminhoco');
       expect(listDogs[1].name).toEqual('Soneca');
     });
