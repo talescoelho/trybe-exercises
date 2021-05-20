29447adbf559fd5e81f6dde0406289f2aa27e562
Tales Coelho <tales-coelho@live.com>
2021-05-20 11:46:20 -0300

    Exercicio de fixação 6.1 e 6.2

--- bloco-10/dia-2/tests/exercicioDoDia.test.js
+++ bloco-10/dia-2/tests/exercicioDoDia.test.js
@@ -48,4 +48,45 @@ it(`Teste Requisição válida git`, async () => {
   expect(gitRepositoriesTodoList).toContain('sd-01-week4-5-project-meme-generator')
 })
 
-// https://api.github.com/orgs/tryber/repos
+// Exercício 6
+const { getAnimal, getAnimalAge } = require('../exercicioDoDia/findAnimalByName')
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimalAge(1).then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimalAge(3).catch(error =>
+        expect(error).toEqual('Nenhum animal com essa Idade!')
+      );
+    });
+  });
+});
