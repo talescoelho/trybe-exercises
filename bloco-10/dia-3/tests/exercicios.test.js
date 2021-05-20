e396e7784a116e6146f82aae5126cf8282381b49
Tales Coelho <tales-coelho@live.com>
2021-05-20 19:39:53 -0300

    Exercicio 5 feito

--- bloco-10/dia-3/tests/exercicios.test.js
+++ bloco-10/dia-3/tests/exercicios.test.js
@@ -53,7 +53,7 @@ describe('#Exercício 1, 2 e 3', () => {
 });
 
 let stringFnc = require('../exercicioDoDia/stringTransform');
-describe('#Exercício 4', () => {
+describe('#Exercício 4 e 5', () => {
   // #4 Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.
   test("#4 Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.", () => {
     const mocStringTransform = jest
@@ -85,6 +85,14 @@ describe('#Exercício 4', () => {
   });
   
   // #5 Utilizando as mesmas funções do exercício anterior, repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar.
-  
+  test("#5 Utilizando as mesmas funções do exercício anterior, repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar.", () => {
+    stringFnc.textToUpperCase.mockRestore();
+    stringFnc.firstLength.mockRestore();
+    stringFnc.concatStrings.mockRestore();
+    
+    expect(stringFnc.textToUpperCase('TaLes')).toBe('TALES');
+    expect(stringFnc.firstLength('TaLes')).toBe('T');
+    expect(stringFnc.concatStrings('Tales', 'Coelho')).toBe('Tales Coelho');
+  });
 })
 // #6 Crie uma função que faça requisição para a api dog pictures . Mocke a requisição e crie dois testes. O primeiro deve interpretar que a requisição se resolveu e teve como valor "request sucess". O segundo deve interpretar que a requisição falhou e ter como valor "request failed". Crie todos os testes que achar necessário.
