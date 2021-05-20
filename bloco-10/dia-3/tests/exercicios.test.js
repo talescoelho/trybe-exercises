740f46dd49611ad7efc582658be730e0e6709d0b
Tales Coelho <tales-coelho@live.com>
2021-05-20 18:57:59 -0300

    Exercicio 1 refatorado para spyOn e exercício 2 resolvido

--- bloco-10/dia-3/tests/exercicios.test.js
+++ bloco-10/dia-3/tests/exercicios.test.js
@@ -4,7 +4,7 @@ let functionNumber = require('../exercicioDoDia/randNumber');
 test("#1 Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.", () => {
   const mockRandNumber = jest
     .spyOn(functionNumber, "randNumber")
-    .mockReturnValue(20);
+    .mockReturnValue(10);
     
     mockRandNumber();
   expect(mockRandNumber).toHaveBeenCalled();
@@ -14,15 +14,17 @@ test("#1 Crie uma função que gere um número aleatório entre 0 e 100. Você i
 
 // #2 Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
 
-// test("#2 Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.", () => {
-//   functionNumber.randNumber
-//     .mockImplementation((a, b) => a / b);
-
-//   functionNumber.randNumber(4 / 2);
-//   expect(functionNumber.randNumber).toHaveBeenCalled();
-//   expect(functionNumber.randNumber(4 / 2)).toBe(2);
-//   expect(functionNumber.randNumber).toHaveBeenCalledTimes(2);
-// });
+test("#2 Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.", () => {
+  functionNumber.randNumber.mockClear();
+  const mockRandNumber = jest
+    .spyOn(functionNumber, "randNumber")
+    .mockImplementation((a, b) => a / b);
+    
+    mockRandNumber();
+  expect(mockRandNumber).toHaveBeenCalled();
+  expect(mockRandNumber(20, 2)).toBe(10);
+  expect(mockRandNumber).toHaveBeenCalledTimes(2);
+});
 
 // #3 Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
 
