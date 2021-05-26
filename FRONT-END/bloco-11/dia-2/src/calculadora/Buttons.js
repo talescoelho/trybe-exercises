e1d332b2a3ce7fbdf503d2cd6deae800209ac45b
Tales Coelho <tales-coelho@live.com>
2021-05-26 09:26:31 -0300

    Estilizando minha calculadora

--- FRONT-END/bloco-11/dia-2/src/calculadora/Buttons.js
+++ FRONT-END/bloco-11/dia-2/src/calculadora/Buttons.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function Buttons(key) {
-  // console.log(key);
   return (
     <button key={key.value} onClick={key.click} className={`button-${key.value}`} >
       {key.value}
--- FRONT-END/bloco-11/dia-2/src/calculadora/Calculadora.js
+++ FRONT-END/bloco-11/dia-2/src/calculadora/Calculadora.js
@@ -5,25 +5,28 @@ import './calculator.css'
 function Calculadora() {
   const [state, setState] = React.useState('');
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-  const operations = ['+', '-', '*', '/']
+  const operations = ['+', '-', 'x', '/', '^', '%']
   return (
-    <div class='calc'>
+    <div className='calc' >
       <div className='calc-display' >{state === '' ? 0 : state}</div>
 
-      <div className='calc-buttons'>
-        <div className='calc-operators' >
+      <div className='calc-buttons' >
+        <div className='calc-operators' key='calc-operators'>
           {operations.map((op) => <Buttons value={op} click={() => isNaN(Number(state.substr(-1)))
             ? setState(state.split(' ').map((v, i, a) => v === a[a.length - 1] ? op : v).join(' '))
             : setState(`${state} ${op}`)} />)}
+          <Buttons value={'AC'} click={() => setState('')} />
         </div>
-        <div className='calc-numbers'>
-          {numbers.map((number) => <Buttons value={number} click={() => setState(state === 'Syntax Error' ?  number : state + number)} />)}
-        </div>
+        <div className='calc-numbers' key='calc-numbers'>
+          {numbers.map((number) => <Buttons value={number} click={() => setState(state === 'Syntax Error' 
+            ? number 
+            : state + number)} />)}
+         </div>
       </div>
 
       <div className='calc-btn-result' >
         <Buttons value={'Calcular'} click={() => setState(state.split(' ').reduce((acc, el) => {
-          if ( acc === 'Syntax Error') {
+          if (acc === 'Syntax Error') {
             return acc;
           }
           if (el.substring(0, 1) === '/') {
@@ -35,8 +38,12 @@ function Calculadora() {
             acc = Number(acc) + Number(el.substring(1))
           } else if (el.substring(0, 1) === '-') {
             acc = Number(acc) - Number(el.substring(1))
-          } else if (el.substring(0, 1) === '*') {
+          } else if (el.substring(0, 1) === 'x') {
             acc = Number(acc) * Number(el.substring(1))
+          } else if (el.substring(0, 1) === '^') {
+            acc = Number(acc) ** Number(el.substring(1))
+          } else if (el.substring(0, 1) === '%') {
+            acc = Number(acc) * Number(el.substring(1)) / 100
           }
           return `${acc}`;
         }))} />
